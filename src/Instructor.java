019aa537115d8cae509f40b3b8989f665b7b6db2
Fina <fpang6@uwo.ca>
2017-02-15 21:01:33 -0500

    Modified Student and Instructor.

--- src/Instructor.java
+++ src/Instructor.java
@@ -7,7 +7,8 @@ public class Instructor extends Person {
 
     public Instructor(String name, String emailAddress, String title, Course favCourse){
         super(name,emailAddress);
-        this.title=title;
+        this.title = title;
+        this.favCourse = favCourse;
     }
 
     public String toString(){
